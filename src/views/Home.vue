75750ab98f5218579842b3f62a278c1c2a239c30
sdimichele <sabrinadimichele1992@gmail.com>
2019-06-03 11:10:49 -0500

    added buttons

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,83 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+     <h1> New Product </h1>
+     <div>
+       Name: <input v-model="newProductName">
+     </div>
+     <div>
+       Description: <input v-model="newProductDescription">
+     </div>
+     <div>
+       Price: <input v-model="newProductPrice">
+     </div>
+     <div>
+       Image: <input v-model="newProductImageUrl">
+     </div>
+
+    <button v-on:click="createProduct()"> Create </button>
+
+    <h1> All Products </h1>
+
+    <div v-for="product in products">
+      <img v-bind:src="product.image_url" v-bind:alt="product.name">
+      <h1> Name: {{product.name }}</h1>
+      <div v-if="currentProduct === product">
+        <h2> Description: {{product.description}}</h2>
+        <h2> Price: {{product.price}}</h2>
+      </div>
+      <button v-on:click="currentProduct = product"> More Info</button>
+    </div>
+    
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
+<style>
+  img {
+        width: 250px;
+      }
+</style>
+
+<script>
+  var axios = require('axios');
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  data: function() {
+    return {
+      products: [],
+      newProductName: "",
+      newProductDescription: "",
+      newProductPrice: "",
+      newProductImageUrl: "",
+      currentProduct: {}
+    };
+  },
+  created: function() {
+
+  axios.get("/api/products").then(response => {
+    this.products = response.data;
+  });
+  },
+  methods: {
+    createProduct: function(){
+      console.log("Create the product");
+
+      var params = {
+                    name: this.newProductName,
+                    description: this.newProductDescription,
+                    price: this.newProductPrice,
+                    image_url: this.newProductImageUrl
+      };
+      axios.post("/api/products", params).then(response => {
+        console.log("Success", response.data);
+        this.products.push(response.data);
+
+        this.newProductName = "";
+        this.newProductDescription = "";
+        this.newProductPrice= "";
+        this.newProductImageUrl= "";
+      });
+      
+    }
   }
-}
+};
 </script>
